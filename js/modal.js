2413c6100afe0262cfa767d46f788129d0ade54e
Oseias Beu <oseiasbeu@outlook.com>
2018-04-15 10:56:03 -0300

    Add files via upload
    Modal incluso em todas as imagens

--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,37 @@
-let modal = document.getElementById('myModal');
-let btn = document.getElementById("myBtn");
-let btn1 = document.getElementById("myBtn1");
-let span = document.getElementsByClassName("close")[0];
+// let modal = document.getElementById('myModal');
+// let btn = document.getElementById("myBtn");
+// let btn1 = document.getElementById("myBtn1");
+// let span = document.getElementsByClassName("close")[0];
 
 
-btn.onclick = function() {
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+function clique(img) {
+    let modal = document.getElementById('janelaModal');
+    let modalImg = document.getElementById("imgModal");
+    let captionTexto = document.getElementById("txtImg");
+    let btFechar = document.getElementsByClassName("fechar")[0];
+
     modal.style.display = "block";
-}
+    modalImg.src = img.src;
+    modalImg.alt = img.alt;
+    captionTexto.innerHTML = img.alt;
 
-
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-
-window.onclick = function(event) {
-    if (event.target == modal) {
+    btFechar.onclick = function() {
         modal.style.display = "none";
     }
 }
